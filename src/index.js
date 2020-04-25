991acfabbd7e829477d078fa229da52b2a7b0e4b
michaelcurry95 <fender18295@gmail.com>
2020-04-25 10:05:53 -0400

    prettier

--- src/index.js
+++ src/index.js
@@ -5,18 +5,18 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.dbUri,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  },
-  (err) => {
-    if (!err) {
-      console.log("connected to db!");
-    } else {
-      console.log("db error:\n", err);
-    }
-  }
+	process.env.dbUri,
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	},
+	(err) => {
+		if (!err) {
+			console.log("connected to db!");
+		} else {
+			console.log("db error:\n", err);
+		}
+	}
 );
 
 const router = require("./router");
@@ -24,7 +24,7 @@ const router = require("./router");
 const app = express();
 
 app.use(bodyParser.json({ type: "*/*" }));
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 router(app);
 
