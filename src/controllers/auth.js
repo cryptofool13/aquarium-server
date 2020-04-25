e9d79501687c2526ed13223eb3b43dfa32cda10f
michaelcurry95 <fender18295@gmail.com>
2020-04-25 10:05:42 -0400

    pre-save hook only hashed pw if user is new, login returns expected result

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,22 +1,22 @@
 const User = require("../model/user");
 
 exports.createUser = function (req, res, next) {
-  let newUser = req.body;
-  User.create({ name: newUser.name, password: newUser.password }).then(
-    (user) => {
-      user.save((e, doc) => {
-        console.log(doc);
-        res.send(doc);
-      });
-    }
-  );
+	let newUser = req.body;
+	User.create({
+		name: newUser.name,
+		password: newUser.password,
+	}).then((user) => {
+		user.save((e, doc) => {
+			res.send(doc);
+		});
+	});
 };
 
 exports.logIn = function (req, res, next) {
-  let returningUser = req.body;
+	let returningUser = req.body;
 
-  User.findOne({ name: returningUser.name }).then((err, user) => {
-    console.log(user)
-    user.comparePassword(returningUser.password, console.log);
-  });
+	User.findOne({ name: returningUser.name }).then((user) => {
+		user.comparePassword(returningUser.password, console.log);
+		res.send(user);
+	});
 };
