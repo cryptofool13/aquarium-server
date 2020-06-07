b7545bf1c9da7a08701b80a9be0b2d6e7aae89db
cryptofool13 <fender18295@gmail.com>
2020-06-07 16:40:29 -0400

    new routes

--- src/router.js
+++ src/router.js
@@ -1,10 +1,14 @@
 const { index } = require("./middlewares");
 
-const { createUser, logIn } = require("./controllers/auth");
+const { createUser, logIn, verifyToken, getUserData } = require("./controllers/auth");
+const {newTank} = require('./controllers/tank')
 
 module.exports = (app) => {
   // send api docs inside index.html on '/'
   app.get("/", index);
   app.post("/signup", createUser);
   app.post("/login", logIn);
+  app.post("/verify", verifyToken);
+  app.post("/tank/new", newTank);
+  app.get("/user", getUserData)
 };
